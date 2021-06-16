a695cfd87763ac501ec899787f0ea18c3992ac8d
rookie7my <rookie2000g@gmail.com>
2021-06-16 21:11:11 +0900

    Initial commit

---
+++ src/main/java/everylog/EverylogApplication.java
@@ -0,0 +1,13 @@
+package everylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverylogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EverylogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/everylog/EverylogApplicationTests.java
@@ -0,0 +1,13 @@
+package everylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverylogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
