43d097d5510bbdbb6bad60cdb693a2a7ba84a2a2
rookie7my <rookie2000g@gmail.com>
2021-06-17 22:31:06 +0900

    Add account's home view

---
+++ src/main/java/everylog/controller/AccountController.java
@@ -0,0 +1,17 @@
+package everylog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class AccountController {
+
+    @GetMapping("/@{name}")
+    public String accountHome(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "account/home";
+    }
+
+}
