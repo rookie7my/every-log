8a563aac37aa739664f3b3d214c50b483c73144c
rookie7my <rookie2000g@gmail.com>
2021-06-22 22:01:30 +0900

    Implement UserDetailsService

--- src/main/java/everylog/repository/AccountRepository.java
+++ src/main/java/everylog/repository/AccountRepository.java
@@ -11,4 +11,6 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     boolean existsByEmail(String email);
 
     Account findByUsername(String username);
+
+    Account findByEmail(String email);
 }
---
+++ src/main/java/everylog/service/AccountService.java
@@ -0,0 +1,35 @@
+package everylog.service;
+
+import everylog.domain.Account;
+import everylog.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        Account account = accountRepository.findByUsername(usernameOrEmail);
+        if(account == null) {
+            account = accountRepository.findByEmail(usernameOrEmail);
+        }
+
+        if(account == null) {
+            throw new UsernameNotFoundException(usernameOrEmail);
+        }
+
+        return User.builder()
+                .username(account.getUsername())
+                .password(account.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
