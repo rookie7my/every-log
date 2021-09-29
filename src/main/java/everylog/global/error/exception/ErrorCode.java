995cf6a271e94c7c0a564de7400ad899807f0ef1
rookie7my <rookie2000g@gmail.com>
2021-09-29 21:36:52 +0900

    Add ErrorCode to ErrorResult to identify Error

---
+++ src/main/java/everylog/global/error/exception/ErrorCode.java
@@ -0,0 +1,11 @@
+package everylog.global.error.exception;
+
+public enum ErrorCode {
+    COMMENT_1,
+    COMMENT_2,
+
+    ACCOUNT_1,
+    ACCOUNT_2,
+
+    BLOG_1
+}
--- src/main/java/everylog/global/error/exception/ErrorResult.java
+++ src/main/java/everylog/global/error/exception/ErrorResult.java
@@ -4,23 +4,24 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import static everylog.global.error.exception.ErrorCode.*;
 import static org.springframework.http.HttpStatus.*;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorResult {
 
-    INVALID_WRITER_ID_FOR_COMMENT_CREATION(BAD_REQUEST,
+    INVALID_WRITER_ID_FOR_COMMENT_CREATION(BAD_REQUEST, COMMENT_1,
             "when creating comment, writer with given writerId does not exist"),
-    INVALID_BLOG_POST_ID_FOR_COMMENT_CREATION(BAD_REQUEST,
+    INVALID_BLOG_POST_ID_FOR_COMMENT_CREATION(BAD_REQUEST, COMMENT_2,
             "when creating comment, blogPost with given blogPostId does not exist"),
 
+    ACCOUNT_NOT_FOUND(NOT_FOUND, ACCOUNT_1,"Requested account can not be found"),
+    INVALID_CURRENT_ACCOUNT_ID(INTERNAL_SERVER_ERROR, ACCOUNT_2,"currentAccountId is invalid"),
 
-    ACCOUNT_NOT_FOUND(NOT_FOUND, "Requested account can not be found"),
-    INVALID_CURRENT_ACCOUNT_ID(INTERNAL_SERVER_ERROR, "currentAccountId is invalid"),
-
-    BLOG_POST_NOT_FOUND(NOT_FOUND, "Requested blogPost can not be found");
+    BLOG_POST_NOT_FOUND(NOT_FOUND, BLOG_1,"Requested blogPost can not be found");
 
     private final HttpStatus httpStatus;
+    private final ErrorCode errorCode;
     private final String message;
 }
